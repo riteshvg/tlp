d8800970d80824fe55f3fc28b2aba4bc8aa3a3c6
riteshvg <riteshvgupta@gmail.com>
2025-01-19 19:36:49 +0530

    commit for jan 19, 2025

--- static/assets/js/expandingcards.js
+++ static/assets/js/expandingcards.js
@@ -1,14 +1,15 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel');
 
-panels.forEach(function(panel){
-    panel.addEventListener('click', function(){
-        removeActiveClassList();
-        panel.classList.add('active')
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
     })
 })
 
-function removeActiveClassList() {
-    panels.forEach(function(panel){
-        panel.classList.remove('active')
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+        //panel.style.backgroundColor = 'aquamarine'
     })
 }
