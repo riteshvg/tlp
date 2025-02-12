87349da0845c226e8060e48c4d4a061c47c78c75
riteshvg <riteshvgupta@gmail.com>
2025-02-12 19:55:39 +0530

    commit for feb 12, 2025

--- public/assets/js/faqscript.js
+++
@@ -1,7 +0,0 @@
-const toggles = document.querySelectorAll('.faq-toggle')
-
-toggles.forEach(function(toggle){
-    toggle.addEventListener('click', function() {
-        toggle.parentNode.classList.toggle('active')
-    })
-})
